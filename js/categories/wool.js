fba4e17b460144e6bcf4a30c9860a183f442eca5
AQUIL the eagle <160960230+AQUILtheeagle@users.noreply.github.com>
2025-11-30 14:41:35 +0100

    Add files via upload

---
+++ js/categories/wool.js
@@ -0,0 +1,29 @@
+const article_wool = {
+    id: "wool",
+    title: "Wool",
+    description: "Wool is an essential primary resource for advanced survival and infrastructural expansion in the game CharCrafter. Although primarily obtained through looting the Sheep, its core value lies in its versatility as a material for creating insulating fabrics and strategic tools.",
+    content: `
+        <p>Wool is an essential primary resource for advanced survival and infrastructural expansion in the game CharCrafter. Although primarily obtained through looting the Sheep, its core value lies in its versatility as a material for creating insulating fabrics and strategic tools.</p>
+        
+        <h3 class="pixel-text" style="color: #00ff00; margin-top: 20px;">TRANSFORMATION</h3>
+        <p>TThe utility of wool is conditional upon its transformation into a Ball of Wool. This intermediate step is essential before accessing most advanced recipes. Note that the Water Filter requires raw Wool (wool: 3), not the Ball of Wool intermediate.</p>
+        <h5 class="pixel-text" style="color: #00ff00; margin-top: 20px;">RECIPE & CRAFTING TIME</h5>
+        <p>Four units of Wool (wool: 4) are required to produce a single Ball of Wool (ball of wool: 1). The process demands between 30000 and 40000 seconds of work.</p>
+        
+        <h3 class="pixel-text" style="color: #00ff00; margin-top: 20px;">RELATED ITEMS</h3>
+        <p>Below, you can find all the items related to wool:</p>
+        <ul class="wiki-list">
+            <li>Wool</li>
+			<li>Ball Of Wool</li>
+			<li>Water Filter</li>
+        </ul>
+	`,
+    infobox: {
+        "Objects Name": "Wool",
+        "Primary Origin Animal(s)": "Sheep",
+        "Item Category": "Animal/Base Resource",
+        "Loot Amount": "4 units of Wool per Sheep defeat",
+        "Base Conversion": "Ball of Wool (requires 4x raw Wool)",
+        "Conversion Time": "30000 - 40000 seconds"
+    }
+};
